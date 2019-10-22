52938246b76cc78ae01b735a283a7061011903a8
hardbirck21 <houlixuan@hotmail.com>
2019-10-23 00:30:50 +0800

    push

--- assets/live2d/autoload.js
+++ assets/live2d/autoload.js
@@ -1,6 +1,7 @@
 try {
     $("<link>").attr({href: "assets/live2d/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
-    $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+    $('body').append('<div class="waifu" id="myLive2d" style="right: 0;bottom: 20px;z-index: 520;position: fixed;pointer-events: none;">'+
+    '<div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({url: 'assets/live2d/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
     $.ajax({url: 'assets/live2d/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
