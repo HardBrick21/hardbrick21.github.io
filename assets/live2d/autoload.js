915f6d510ef881a4cb6c0f61672eecef88873a24
hardbirck21 <houlixuan@hotmail.com>
2019-10-23 12:59:09 +0800

    push

--- assets/live2d/autoload.js
+++ assets/live2d/autoload.js
@@ -1,9 +1,9 @@
 try {
 
-    $("<link>").attr({href: getRootPath()+"assets/live2d/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: getRootPath()+"/assets/live2d/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu" id="myLive2d" style="bottom: 20px;z-index: 520;position: fixed;pointer-events: none;"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: getRootPath()+'assets/live2d/waifu-tips.js?v=1.4.2',dataType:"script", cache: true, async: false});
-    $.ajax({url: getRootPath()+'assets/live2d/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+    $.ajax({url: getRootPath()+'/assets/live2d/waifu-tips.js?v=1.4.2',dataType:"script", cache: true, async: false});
+    $.ajax({url: getRootPath()+'/assets/live2d/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
     live2d_settings['modelId'] = 2;                  // 默认模型 ID
@@ -11,7 +11,7 @@ try {
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
     live2d_settings['waifuEdgeSide'] = 'right:40';
     /* 在 initModel 前添加 */
-    initModel('assets/live2d/waifu-tips.json');
+    initModel(getRootPath()+'/assets/live2d/waifu-tips.json');
 } catch(err) { console.log('[Error] JQuery is not defined.') }
 
 
