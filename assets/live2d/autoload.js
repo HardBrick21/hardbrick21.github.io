fe9f917788790b7a6ee9723b6bff6248c59f958d
hardbirck21 <houlixuan@hotmail.com>
2019-10-23 12:12:25 +0800

    push

--- assets/live2d/autoload.js
+++ assets/live2d/autoload.js
@@ -1,4 +1,5 @@
 try {
+
     $("<link>").attr({href: "assets/live2d/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu" id="myLive2d" style="bottom: 20px;z-index: 520;position: fixed;pointer-events: none;"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({url: 'assets/live2d/waifu-tips.js?v=1.4.2',dataType:"script", cache: true, async: false});
@@ -8,6 +9,7 @@ try {
     live2d_settings['modelId'] = 2;                  // 默认模型 ID
     live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+    live2d_settings['waifuEdgeSide'] = 'right:40'; 
     /* 在 initModel 前添加 */
     initModel('assets/live2d/waifu-tips.json');
 } catch(err) { console.log('[Error] JQuery is not defined.') }
